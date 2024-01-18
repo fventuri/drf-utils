2d3c9c609a17ffba536173a4d92037eec3cb7d1e
Franco Venturi <fventuri@comcast.net>
2024-01-17 20:44:16 -0500

    wav2drf for 24-hours 10sps wav files

---
+++ wav2drf-old.py
@@ -0,0 +1,143 @@
+#!/usr/bin/python3 -u
+# convert one or more wav files into a Digital RF dataset
+#
+# Copyright 2023 Franco Venturi K4VZ
+#
+# Version: 1.0 - Tue Dec 26 05:47:14 PM EST 2023
+
+from datetime import datetime
+import digital_rf as drf
+import getopt
+import numpy as np
+import os
+import soundfile as sf
+import sys
+import uuid
+
+
+
+# Digital RF settings
+drf_channel_name = 'ch0'
+drf_subdir_cadence_secs = 21600
+drf_file_cadence_millisecs = 3600000
+drf_compression_level = 9
+
+
+def main():
+    drfdir = None
+    frequencies = []
+    start_time = 0
+    # location (default: KFS)
+    latitude = 37.444722
+    longitude = -122.1125
+    uuid_str = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'd:f:s:l:u:')
+    except getopt.GetoptError as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+    for o, a in opts:
+        if o == '-d':
+            drfdir = a
+        elif o == '-f':
+            frequencies.extend([float(x) for x in a.split(',')])
+        elif o == '-s':
+            start_time = datetime.fromisoformat(a).timestamp()
+        elif o == '-l':
+            latitude, longitude = [float(x) for x in a.split(',')]
+        elif o == '-u':
+            uuid_str = a
+
+    if not args:
+        print('missing input files', file=sys.stderr)
+        sys.exit(1)
+
+    if drfdir is None:
+        print('missing Digital RF output dir (-d) option', file=sys.stderr)
+        sys.exit(1)
+
+    if len(frequencies) != len(args):
+        print(f'number of frequencies {len(frequencies)} does not match with number of input files {len(args)}', file=sys.stderr)
+        sys.exit(1)
+
+    sample_rate = None
+    num_samples = None
+    num_channels = None
+    data_type = None
+    samples = [None] * len(args)
+    for idx, wav_file in enumerate(args):
+        samples[idx], wav_sample_rate = sf.read(wav_file, dtype='float32')
+
+        # sanity checks
+        if sample_rate == None:
+            sample_rate = wav_sample_rate
+        elif wav_sample_rate != sample_rate:
+            printf('sample rates do not match - file', wav_file, 'has', wav_sample_rate, '- expecting:', sample_rate, file=sys.stderr)
+            sys.exit(1)
+        wav_num_samples = samples[idx].shape[0]
+        if num_samples == None:
+            num_samples = wav_num_samples
+        elif wav_num_samples != num_samples:
+            printf('number of samples does not match - file', wav_file, 'has', wav_num_samples, '- expecting:', num_samples, file=sys.stderr)
+            sys.exit(1)
+        wav_num_channels = samples[idx].shape[1]
+        if num_channels == None:
+            num_channels = wav_num_channels
+        elif wav_num_channels != num_channels:
+            printf('number of channels does not match - file', wav_file, 'has', wav_num_channels, '- expecting:', num_channels, file=sys.stderr)
+            sys.exit(1)
+        wav_data_type = samples[idx].dtype
+        if data_type == None:
+            data_type = wav_data_type
+        elif wav_data_type != data_type:
+            printf('data type does not match - file', wav_file, 'has', wav_data_type, '- expecting:', data_type, file=sys.stderr)
+            sys.exit(1)
+
+    start_global_index = int(start_time * sample_rate)
+    if uuid_str is None:
+        uuid_str = uuid.uuid4().hex
+
+    # the output directory must already exist
+    chdir = os.path.join(drfdir, drf_channel_name)
+    os.makedirs(chdir)
+    metadatadir = os.path.join(chdir, 'metadata')
+    os.mkdir(metadatadir)
+
+    # write Digital RF metadata first
+    do = drf.DigitalMetadataWriter(metadatadir,
+                                   drf_subdir_cadence_secs,
+                                   drf_subdir_cadence_secs,  # file_cadence_secs
+                                   sample_rate,      # sample_rate_numerator 
+                                   1,                # sample_rate_denominator
+                                   'metadata'        # file_name
+                                  )
+    data_dict = {
+        'uuid_str': uuid_str,
+        'lat': np.single(latitude),
+        'long': np.single(longitude),
+        'center_frequencies': np.ascontiguousarray(frequencies)
+    }
+    do.write(start_global_index, data_dict)
+
+    # write Digital RF data
+    with drf.DigitalRFWriter(chdir,
+                             data_type,
+                             drf_subdir_cadence_secs,
+                             drf_file_cadence_millisecs,
+                             start_global_index,
+                             sample_rate,            # sample_rate_numerator
+                             1,                      # sample_rate_denominator
+                             uuid_str,
+                             drf_compression_level,
+                             False,                  # checksum
+                             num_channels == 2,      # is_complex
+                             len(args),              # num_subchannels
+                             True,                   # is_continuous
+                             False                   # marching_periods
+                            ) as do:
+        do.rf_write(np.hstack(samples, casting='no'))
+    print('done', file=sys.stderr)
+
+
+if __name__ == '__main__':
+    main()
--- wav2drf.py
+++ wav2drf.py
@@ -1,142 +1,234 @@
 #!/usr/bin/python3 -u
-# convert one or more wav files into a Digital RF dataset
+# convert a directory tree containing wav files into a Digital RF dataset
+# directory structure:
+#   - topdir (input dir: -i)
+#     - subdir1 (subchannel 1) - frequency is now derived from channel name
+#       - <filename>.wav
+#     - subdir2 (subchannel 2) - frequency is now derived from channel name
+#       - <filename>.wav
+#     ...
 #
-# Copyright 2023 Franco Venturi K4VZ
+# Copyright 2024 Franco Venturi K4VZ
 #
-# Version: 1.0 - Tue Dec 26 05:47:14 PM EST 2023
+# Version: 1.0 - Tue Jan 16 09:46:22 PM EST 2024
 
-from datetime import datetime
+from collections import defaultdict
+from configparser import ConfigParser
+from datetime import datetime, timezone
 import digital_rf as drf
 import getopt
 import numpy as np
 import os
+import re
 import soundfile as sf
 import sys
 import uuid
 
+# global variables
+verbose = 0
 
 
-# Digital RF settings
-drf_channel_name = 'ch0'
-drf_subdir_cadence_secs = 21600
-drf_file_cadence_millisecs = 3600000
-drf_compression_level = 9
+def get_subchannels(inputdir, subdir2freq):
+    # create list of subchannels and make sure that each has one wav file in it
+    subchannels = []
+    for subdir in os.listdir(inputdir):
+        if subdir not in subdir2freq:
+            print('Subdir', subdir, 'not found in subchannels list. Skipping it.', file=sys.stderr)
+            continue
+        subdir_content = os.listdir(os.path.join(inputdir, subdir))
+        if not (len(subdir_content) == 1 and subdir_content[0].endswith('.wav')):
+            print('Subdir', subdir, 'does not contain a single wav file. Skipping it.', f'(content: {subdir_content})', file=sys.stderr)
+            continue
+        subchannels.append((subdir, subdir_content[0], float(subdir2freq[subdir])))
+    # return subchannels in ascending frequency order
+    subchannels.sort(key=lambda x: x[2])
+    return subchannels
 
 
-def main():
-    drfdir = None
-    frequencies = []
-    start_time = 0
-    # location (default: KFS)
-    latitude = 37.444722
-    longitude = -122.1125
-    uuid_str = None
-    try:
-        opts, args = getopt.getopt(sys.argv[1:], 'd:f:s:l:u:')
-    except getopt.GetoptError as ex:
-        print(ex, file=sys.stderr)
-        sys.exit(1)
-    for o, a in opts:
-        if o == '-d':
-            drfdir = a
-        elif o == '-f':
-            frequencies.extend([float(x) for x in a.split(',')])
-        elif o == '-s':
-            start_time = datetime.fromisoformat(a).timestamp()
-        elif o == '-l':
-            latitude, longitude = [float(x) for x in a.split(',')]
-        elif o == '-u':
-            uuid_str = a
+def create_drf_dataset(inputdir, outputdir, subchannels, config_global, start_time, uuid_str=None):
+    channel_name = config_global['channel name']
+    subdir_cadence_secs = int(config_global['subdir cadence secs'])
+    file_cadence_millisecs = int(config_global['file cadence millisecs'])
+    compression_level = int(config_global['compression level'])
+    dtype = 'i2'  # 16bit shorts
+    #dtype = 'float32'
 
-    if not args:
-        print('missing input files', file=sys.stderr)
-        sys.exit(1)
+    if uuid_str is None:
+        uuid_str = uuid.uuid4().hex
 
-    if drfdir is None:
-        print('missing Digital RF output dir (-d) option', file=sys.stderr)
-        sys.exit(1)
+    print('writing Digital RF dataset. This will take a while', file=sys.stderr)
 
-    if len(frequencies) != len(args):
-        print(f'number of frequencies {len(frequencies)} does not match with number of input files {len(args)}', file=sys.stderr)
-        sys.exit(1)
-
+    # build np.array with samples and validate wav files to make sure they
+    # are all consistent (same num samples, channels, data type, etc)
     sample_rate = None
     num_samples = None
     num_channels = None
     data_type = None
-    samples = [None] * len(args)
-    for idx, wav_file in enumerate(args):
-        samples[idx], wav_sample_rate = sf.read(wav_file, dtype='float32')
+    samples = [None] * len(subchannels)
+    for idx, subchannel in enumerate(subchannels):
+        wav_file = os.path.join(inputdir, subchannel[0], subchannel[1])
+        samples[idx], wav_sample_rate = sf.read(wav_file, dtype=dtype)
 
         # sanity checks
         if sample_rate == None:
             sample_rate = wav_sample_rate
         elif wav_sample_rate != sample_rate:
             printf('sample rates do not match - file', wav_file, 'has', wav_sample_rate, '- expecting:', sample_rate, file=sys.stderr)
-            sys.exit(1)
+            return False, None, None, None, None
         wav_num_samples = samples[idx].shape[0]
         if num_samples == None:
             num_samples = wav_num_samples
         elif wav_num_samples != num_samples:
             printf('number of samples does not match - file', wav_file, 'has', wav_num_samples, '- expecting:', num_samples, file=sys.stderr)
-            sys.exit(1)
+            return False, None, None, None, None
         wav_num_channels = samples[idx].shape[1]
         if num_channels == None:
             num_channels = wav_num_channels
         elif wav_num_channels != num_channels:
             printf('number of channels does not match - file', wav_file, 'has', wav_num_channels, '- expecting:', num_channels, file=sys.stderr)
-            sys.exit(1)
+            return False, None, None, None, None
         wav_data_type = samples[idx].dtype
         if data_type == None:
             data_type = wav_data_type
         elif wav_data_type != data_type:
             printf('data type does not match - file', wav_file, 'has', wav_data_type, '- expecting:', data_type, file=sys.stderr)
-            sys.exit(1)
+            return False, None, None, None, None
+
+    if verbose >= 1:
+        print('sample_rate:', sample_rate)
+        print('num_samples:', num_samples)
+        print('num_channels:', num_channels)
+        print('data_type:', data_type)
+        print('len(samples):', len(samples))
 
     start_global_index = int(start_time * sample_rate)
     if uuid_str is None:
         uuid_str = uuid.uuid4().hex
 
     # the output directory must already exist
-    chdir = os.path.join(drfdir, drf_channel_name)
-    os.makedirs(chdir)
-    metadatadir = os.path.join(chdir, 'metadata')
-    os.mkdir(metadatadir)
+    channel_dir = os.path.join(outputdir, channel_name)
+    os.makedirs(channel_dir)
 
-    # write Digital RF metadata first
-    do = drf.DigitalMetadataWriter(metadatadir,
-                                   drf_subdir_cadence_secs,
-                                   drf_subdir_cadence_secs,  # file_cadence_secs
-                                   sample_rate,      # sample_rate_numerator 
-                                   1,                # sample_rate_denominator
-                                   'metadata'        # file_name
-                                  )
-    data_dict = {
-        'uuid_str': uuid_str,
-        'lat': np.single(latitude),
-        'long': np.single(longitude),
-        'center_frequencies': np.ascontiguousarray(frequencies)
-    }
-    do.write(start_global_index, data_dict)
-
-    # write Digital RF data
-    with drf.DigitalRFWriter(chdir,
-                             data_type,
-                             drf_subdir_cadence_secs,
-                             drf_file_cadence_millisecs,
+    with drf.DigitalRFWriter(channel_dir,
+                             dtype,
+                             subdir_cadence_secs,
+                             file_cadence_millisecs,
                              start_global_index,
                              sample_rate,            # sample_rate_numerator
                              1,                      # sample_rate_denominator
                              uuid_str,
-                             drf_compression_level,
+                             compression_level,
                              False,                  # checksum
                              num_channels == 2,      # is_complex
-                             len(args),              # num_subchannels
+                             len(subchannels),       # num_subchannels
                              True,                   # is_continuous
                              False                   # marching_periods
                             ) as do:
+
         do.rf_write(np.hstack(samples, casting='no'))
-    print('done', file=sys.stderr)
+
+    # hopefully deleting samples will free all the memory
+    del samples
+
+    return True, channel_dir, sample_rate, start_global_index, uuid_str
+
+
+def create_drf_metadata(channel_dir, frequencies, config_global, config_metadata, sample_rate, start_global_index, uuid_str):
+    subdir_cadence_secs = int(config_global['subdir cadence secs'])
+    metadatadir = os.path.join(channel_dir, 'metadata')
+    os.makedirs(metadatadir)
+    do = drf.DigitalMetadataWriter(metadatadir,
+                                   subdir_cadence_secs,
+                                   subdir_cadence_secs,  # file_cadence_secs
+                                   sample_rate,      # sample_rate_numerator 
+                                   1,                # sample_rate_denominator
+                                   'metadata'        # file_name
+                                  )
+    sample = start_global_index
+    data_dict = {
+        'uuid_str': uuid_str,
+        'lat': np.single(float(config_metadata['latitude'])),
+        'long': np.single(float(config_metadata['longitude'])),
+        'center_frequencies': np.ascontiguousarray(frequencies)
+    }
+    # all all the remaining arguments as strings
+    for k, v in config_metadata.items():
+        if k in ['latitude', 'longitude']:
+            continue
+        data_dict[k] = v
+    do.write(sample, data_dict)
+    return True
+
+
+def main():
+    configfile = sys.argv[0].replace('.py', '.conf')
+    inputdir = None
+    outputdir = None
+    start_time = 0
+    uuid_str = None
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], 'c:i:o:s:u:v')
+    except getopt.GetoptError as ex:
+        print(ex, file=sys.stderr)
+        sys.exit(1)
+    for o, a in opts:
+        if o == '-c':
+            configfile = a
+        elif o == '-i':
+            inputdir = a
+        elif o == '-o':
+            outputdir = a
+        elif o == '-s':
+            # allow for time zone (default is local TZ)
+            #start_time = datetime.fromisoformat(a).timestamp()
+            # always UTC
+            start_datetime = datetime.fromisoformat(a).replace(tzinfo=timezone.utc)
+            start_time = start_datetime.timestamp()
+        elif o == '-u':
+            uuid_str = a
+        elif o == '-v':
+            global verbose
+            verbose += 1
+
+    if inputdir is None:
+        print('missing input dir (-i) option', file=sys.stderr)
+        sys.exit(1)
+
+    if outputdir is None:
+        print('missing output dir (-o) option', file=sys.stderr)
+        sys.exit(1)
+
+    if start_time == 0:
+        for path_element in inputdir.split(os.path.sep):
+            try:
+                start_datetime = datetime.strptime(path_element, '%Y%m%d').replace(tzinfo=timezone.utc)
+                start_time = start_datetime.timestamp()
+                break
+            except ValueError:
+                pass
+
+    config = ConfigParser(interpolation=None)
+    config.optionxform = str
+    config.read(configfile)
+
+    subchannels = get_subchannels(inputdir, config['subchannels'])
+    if len(subchannels) == 0:
+        print("No subchannels (i.e. no wav files) found. Nothing to do.", file=sys.stderr)
+        sys.exit(0)
+    print('N subchannels:', len(subchannels), file=sys.stderr)
+    if verbose >= 1:
+        print('subchannels:', subchannels)
+
+    ok, channel_dir, sample_rate, start_global_index, uuid_str = create_drf_dataset(inputdir, outputdir, subchannels, config['global'], start_time, uuid_str)
+    print('create_drf_dataset returned', ok, file=sys.stderr)
+    if not ok:
+        sys.exit(1)
+
+    frequencies = [float(x[2]) for x in subchannels]
+    ok = create_drf_metadata(channel_dir, frequencies, config['global'], config['metadata'], sample_rate, start_global_index, uuid_str)
+    print('create_drf_metadata returned', ok, file=sys.stderr)
+    if not ok:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
